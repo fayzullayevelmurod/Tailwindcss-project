f72119a21a4a4a03f93160a3e744d263919dce7a
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-10-20 21:43:57 +0500

    project the end

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{js,html}"],
-  // purge: ["./public/**/*.html", "./src/**/*.{js,html}"],
   theme: {
     extend: {},
   },
