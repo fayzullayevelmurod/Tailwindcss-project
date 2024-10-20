0c2897b4314fc1003aef35c334e973b6f0e8adec
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-10-20 18:42:33 +0500

    project template in progress

---
+++ src/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html", "./src/**/*.{js,html}"],
+  // purge: ["./public/**/*.html", "./src/**/*.{js,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
